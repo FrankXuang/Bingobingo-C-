e06d038f22805720d7e4982d142ef5e6fd62fd63
FrankXuang <126735020+FrankXuang@users.noreply.github.com>
2023-03-23 15:53:18 +0800

    Add files via upload

---
+++ Bingobingo/Homework(BingoBingo)/Form2.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Homework_BingoBingo_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tab顯示玩法 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tab顯示玩法.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 277);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "離開";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tab顯示玩法
+            // 
+            this.tab顯示玩法.Controls.Add(this.tabPage1);
+            this.tab顯示玩法.Controls.Add(this.tabPage2);
+            this.tab顯示玩法.Controls.Add(this.tabPage3);
+            this.tab顯示玩法.Controls.Add(this.tabPage4);
+            this.tab顯示玩法.Controls.Add(this.tabPage5);
+            this.tab顯示玩法.Location = new System.Drawing.Point(20, 10);
+            this.tab顯示玩法.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tab顯示玩法.Name = "tab顯示玩法";
+            this.tab顯示玩法.SelectedIndex = 0;
+            this.tab顯示玩法.Size = new System.Drawing.Size(544, 247);
+            this.tab顯示玩法.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Yellow;
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(536, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "基本玩法";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(2, 2);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(533, 220);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "「BINGO BINGO賓果賓果」是一種每五分鐘開獎一次的電腦彩券遊戲。選號範圍為01~80，您可以任意選擇玩1~10個號碼的玩法（稱為「1星」、「2星」、「3星" +
+    "」....、「10星」），每次開獎時，電腦系統將隨機開出20個獎號，您可以依您選擇的玩法和選號進行對獎。如您的選號符合該期任一種中獎情形，即為中獎，並可依規定兌" +
+    "領獎金。";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Yellow;
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(536, 221);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "超級獎號";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(2, 2);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(533, 220);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "「超級獎號」是BINGO BINGO賓果賓果1~10星玩法的加值版，只要您先購買1~10星玩法任一種玩法，都可加購超級獎號玩法。「超級獎號」是以每期開獎獎號的第2" +
+    "0個獎號做為該期的超級獎號，只要您投注的選號中包含該期開出之超級獎號，您即可依超級獎號玩法專屬的獎金表兌領獎金。";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.Yellow;
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Size = new System.Drawing.Size(536, 221);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "猜大小";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(4, 2);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(533, 220);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "「猜大小」為BINGO BINGO賓果賓果的附加玩法，您可就當期BINGO BINGO賓果賓果可能開出之獎號進行預測，認為較小的號碼（01~40號）開出的數目將等" +
+    "於或多於13個號碼者，選擇投注「猜小」；認為較大的號碼（41~80號）開出的數目將等於或多於13個號碼者，選擇投注「猜大」，只要您猜中即為中獎。";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.Yellow;
+            this.tabPage4.Controls.Add(this.label5);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage4.Size = new System.Drawing.Size(536, 221);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "猜單雙";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(2, 2);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(533, 220);
+            this.label5.TabIndex = 3;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label1);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(536, 221);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "按鈕說明";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("標楷體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 220);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "按鈕\"兌獎\"時請先選擇星數以及確認是否要玩猜大小/單雙。\r\n同一個電腦選碼只能玩一次，若要再玩請等時間刷新或是直接按按鈕開獎號碼~";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(571, 314);
+            this.Controls.Add(this.tab顯示玩法);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form2";
+            this.Text = "玩法說明";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.tab顯示玩法.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TabControl tab顯示玩法;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Bingobingo/Homework(BingoBingo)/兌獎.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_BingoBingo_
+{
+
+    public class 兌獎 : Form1
+    {
+        #region 兌獎
+        internal static void 幾星以及數目()
+        {
+            if ((Globar.星數 == 1)|| (Globar.包牌星數 == 1)){ 一星(Globar.winTimes); }
+            else if ((Globar.星數 == 2)|| (Globar.包牌星數 == 2)) { 二星(Globar.winTimes); }
+            else if ((Globar.星數 == 3)|| (Globar.包牌星數 == 3)) { 三星(Globar.winTimes); }
+            else if ((Globar.星數 == 4)|| (Globar.包牌星數 == 4)) { 四星(Globar.winTimes); }
+            else if ((Globar.星數 == 5) || (Globar.包牌星數 == 5)) { 五星(Globar.winTimes); }
+            else if ((Globar.星數 == 6)|| (Globar.包牌星數 == 6)) { 六星(Globar.winTimes); }
+            else if ((Globar.星數 == 7)|| (Globar.包牌星數 == 7)) { 七星(Globar.winTimes); }
+            else if ((Globar.星數 == 8) || (Globar.包牌星數 == 8)) { 八星(Globar.winTimes); }
+            else if ((Globar.星數 == 9) || (Globar.包牌星數 == 9)) { 九星(Globar.winTimes); }
+            else if ((Globar.星數 == 10)|| (Globar.包牌星數 == 10)) { 十星(Globar.winTimes); }
+            else{ MessageBox.Show("很不幸似乎出錯了..."); }
+            猜大小金額();
+            猜單雙金額();
+            賺錢賠錢();
+        }
+        #endregion
+
+        #region 十星
+        internal static void 十星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 10)
+                {
+                    Globar.總金額 = Globar.投注金額 * 200000;
+                }
+                else if (Globar.winTimes == 5)
+                {
+                    Globar.總金額 = Globar.投注金額;
+                }
+                else if (Globar.winTimes == 6)
+                {
+                    Globar.總金額 = Globar.投注金額*10;
+                }
+                else if (Globar.winTimes == 7)
+                {
+                    Globar.總金額 = Globar.投注金額 * 100;
+                }
+                else if (Globar.winTimes == 8)
+                {
+                    Globar.總金額 = Globar.投注金額 * 1000;
+                }
+                else if (Globar.winTimes == 9)
+                {
+                    Globar.總金額 = Globar.投注金額 * 10000;
+                }
+                else if(Globar.winTimes == 0)
+                { Globar.總金額 = 25; }
+                else{Globar.總金額 = -25;}
+            }
+            else 
+            {
+                if (Globar.winTimes == 10)
+                {Globar.總金額 = Globar.投注金額 * 500000;}
+                else if (Globar.winTimes == 9)
+                {Globar.總金額 = Globar.投注金額 * 25000;}
+                else if (Globar.winTimes == 8)
+                { Globar.總金額 = Globar.投注金額 * 2500; }
+                else if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 250; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 30; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 九星
+        internal static void 九星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 9)
+                {Globar.總金額 = Globar.投注金額 * 40000;}
+                else if (Globar.winTimes == 8)
+                {Globar.總金額 = Globar.投注金額 * 4000;}
+                else if (Globar.winTimes == 7)
+                {Globar.總金額 = Globar.投注金額 * 120;}
+                else if (Globar.winTimes == 6)
+                {Globar.總金額 = Globar.投注金額 * 20;}
+                else if (Globar.winTimes == 5)
+                {Globar.總金額 = Globar.投注金額 * 4;}
+                else if (Globar.winTimes == 4)
+                {Globar.總金額 = Globar.投注金額;}
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = 25; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 10)
+                { Globar.總金額 = Globar.投注金額 * 100000; }
+                else if (Globar.winTimes == 9)
+                { Globar.總金額 = Globar.投注金額 * 10000; }
+                else if (Globar.winTimes == 8)
+                { Globar.總金額 = Globar.投注金額 * 300; }
+                else if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 50; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 30; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 八星
+        internal static void 八星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 8)
+                { Globar.總金額 = Globar.投注金額 * 20000; }
+                else if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 800; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 40; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 8; }
+                else if ((Globar.winTimes == 4)|| (Globar.winTimes == 0))
+                { Globar.總金額 = Globar.投注金額; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 8)
+                { Globar.總金額 = Globar.投注金額 * 50000; }
+                else if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 2000; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 100; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 20; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 七星
+        internal static void 七星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 3200; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 120; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 12; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 2; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = 25; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 7)
+                { Globar.總金額 = Globar.投注金額 * 8000; }
+                else if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 300; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 30; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 5; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 六星
+        internal static void 六星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 1000; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 40; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 8; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = 25; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 6)
+                { Globar.總金額 = Globar.投注金額 * 2500; }
+                else if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 100; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 20; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 五星
+        internal static void 五星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 300; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 20; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 2; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 5)
+                { Globar.總金額 = Globar.投注金額 * 800; }
+                else if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 60; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 6; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 四星
+        internal static void 四星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 40; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 4; }
+                else if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 4)
+                { Globar.總金額 = Globar.投注金額 * 120; }
+                else if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 12; }
+                else if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 三星
+        internal static void 三星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 20; }
+                else if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額 * 2; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 3)
+                { Globar.總金額 = Globar.投注金額 * 60; }
+                else if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額 * 6; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+                else { Globar.總金額 = Globar.投注金額; }
+            }
+        }
+        #endregion
+
+        #region 二星
+        internal static void 二星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 1)
+                { Globar.總金額 = Globar.投注金額 * 1; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 2)
+                { Globar.總金額 = Globar.投注金額 * 8; }
+                else if (Globar.winTimes == 1)
+                { Globar.總金額 = Globar.投注金額 * 3; }
+                else if (Globar.winTimes == 0)
+                { Globar.總金額 = -25; }
+            }
+        }
+        #endregion
+
+        #region 一星
+        internal static void 一星(int 數目)
+        {
+            if (Globar.超級獎項 == false)
+            {
+                if (Globar.winTimes == 1)
+                { Globar.總金額 = Globar.投注金額 * 2; }
+                else { Globar.總金額 = -25; }
+            }
+            else
+            {
+                if (Globar.winTimes == 1)
+                { Globar.總金額 = Globar.投注金額 * 6; }
+                else { Globar.總金額 = -25; }
+            }
+        }
+        #endregion
+
+        #region 猜大小
+        internal static void 猜大小金額()
+        {
+            if (Globar.猜大 == true)
+            {Globar.猜大小金額 = Globar.投注金額 * 6;}
+            else if (Globar.猜小 == true)
+            { Globar.猜大小金額 = Globar.投注金額 * 6; }
+            else if((Globar.猜大 == false)&& (Globar.猜小 == false))
+            { Globar.猜大小金額 = 0; }
+        }
+        #endregion
+
+        #region 猜單雙
+        internal static void 猜單雙金額()
+        {
+            if (Globar.猜單 == true)
+            { Globar.猜單雙金額 = Globar.投注金額 * 6; }
+            else if (Globar.猜小單 == true)
+            { Globar.猜單雙金額 = Globar.投注金額 * 1.8; }
+            else if (Globar.猜小雙 == true)
+            { Globar.猜單雙金額 = Globar.投注金額 * 1.8; }
+            else if (Globar.猜雙 == true)
+            { Globar.猜單雙金額 = Globar.投注金額 * 6; }
+            else if (Globar.猜和 == true)
+            { Globar.猜單雙金額 = Globar.投注金額 * 2.8; }
+            else if ((Globar.猜單 == false) && (Globar.猜小單 == false) && (Globar.猜小雙 == false) && (Globar.猜雙 == false) && (Globar.猜和 == false))
+            { Globar.猜單雙金額 = 0; }
+
+        }
+        #endregion
+
+        #region 賺錢賠錢
+        internal static void 賺錢賠錢()
+        {
+            Globar.全部總金額 = Globar.猜單雙金額 + Globar.猜大小金額 + Globar.總金額;
+            Globar.利潤 += Globar.全部總金額;
+        }
+        #endregion
+    }
+
+}
+
+
